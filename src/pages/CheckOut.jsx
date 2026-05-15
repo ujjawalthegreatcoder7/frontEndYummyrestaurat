df93899a036a497efc53136c5ed04815e70fc407
RADHAji <ujjuarora5@gmail.com>
2026-05-16 01:31:13 +0530

    frontend otp bypass update

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -606,8 +606,8 @@ export default function Checkout() {
       };
 
       const res = await axios.post(
-        // `${BASE_URL}/place-order`,
-        "http://localhost:5000/place-order",
+        `${BASE_URL}/place-order`,
+        // "http://localhost:5000/place-order",
         orderData
       );
 
